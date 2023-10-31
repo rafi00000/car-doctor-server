2e6069a344d9d03dc405b8e1055d0e8da2a1f17c
Rafi <delwar2021bd@gmail.com>
2023-10-31 08:29:13 +0600

    fixed uri issue

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://rafi2021bd:delwar2021bd@cluster0.irfnbkn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.irfnbkn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
